eb488b0dfa8c518db39e2f661085ae2e8e73accc
bogdankozlovsky <bogdan.k@datarails.com>
2022-02-23 17:49:58 +0100

    add alembic and migrations

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from database import Base
 from sqlalchemy import Boolean, Column, DateTime, Integer, String, ForeignKey
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
-#  alembic init alembic
+#  alembic init migrations
 #  alembic revision
 #  alembic revision --autogenerate
 #  alembic upgrade head
@@ -28,7 +28,7 @@ class Item(Base):
 
     id = Column(Integer, primary_key=True)
     title = Column(String(150))
-    # number = Column(Integer, default=0)
+    number = Column(Integer, default=0)
     date_created = Column(DateTime(timezone=True), server_default=func.now())
     user_id = Column(Integer, ForeignKey("users.id"))
 
