59d1547538553aa66450964a83c27a73819c4805
bogdankozlovsky <bogdan.k@datarails.com>
2022-02-24 23:22:47 +0100

    add custom depends

--- dependencies.py
+++ dependencies.py
@@ -3,6 +3,7 @@ from fastapi.security import OAuth2PasswordBearer
 
 from jose import JWTError, jwt
 from datetime import datetime, timezone
+from fastapi import Query
 
 from models import User as UserModel
 from schemas import JWTToken, FullUser
@@ -11,6 +12,14 @@ from utils import hello_world
 
 
 oauth2_passord_bearer = OAuth2PasswordBearer(tokenUrl=tokenUrl)
+
+
+class CustomDepends:
+    def __init__(self, magick_word):
+        self.magick_word = magick_word
+
+    def __call__(self, q: str = Query(None, description="test custom depends")):
+        return q is not None and self.magick_word in q
 
 
 async def get_user(token: str = Depends(oauth2_passord_bearer)) -> FullUser:
@@ -32,8 +41,14 @@ async def get_user(token: str = Depends(oauth2_passord_bearer)) -> FullUser:
     return FullUser.from_orm(user)
 
 
-def get_current_active_user(task: BackgroundTasks, user: FullUser = Depends(get_user)):
+async def get_current_active_user(
+        task: BackgroundTasks,
+        user: FullUser = Depends(get_user),
+        is_query_contain_wisky: bool = Depends(CustomDepends(magick_word="wisky"))
+):
     task.add_task(hello_world, msg=user.username)
+    if is_query_contain_wisky:
+        print(f"{is_query_contain_wisky=}")
     if user.is_active:
         return user
     raise HTTPException(
